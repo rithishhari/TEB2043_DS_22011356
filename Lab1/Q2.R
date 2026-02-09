d063e424537ee3d8a1e8e6d5f6919ddbadec6ed7
Rithish Hari <hiimrithish@gmail.com>
2026-02-09 11:32:02 +0800

    Create Q2.R

---
+++ Lab1/Q2.R
@@ -0,0 +1,11 @@
+# Activity 2 
+
+num1 <- 0.956786
+num2 <- 7.8345901
+
+num1_result <- round(num1, digits = 2)
+
+num2_result <- round(num2, digits = 3)
+
+print(paste("num1 rounded:", num1_result))
+print(paste("num2 rounded:", num2_result))
