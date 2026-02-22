ca8cbd6cda3bcb439ec1bd82b13c99ad3fa42703
Rithish Hari <hiimrithish@gmail.com>
2026-02-23 02:06:38 +0800

    Create Lab4B_Q2.R

---
+++ Lab4/Lab4B_Q2.R
@@ -0,0 +1,20 @@
+V1 <- c(2,3,1,5,4,6,8,7,9)
+
+Matrix1 <- matrix(V1, nrow = 3, ncol = 3)
+
+rownames(Matrix1) <- c("R1","R2","R3")
+colnames(Matrix1) <- c("C1","C2","C3")
+
+Matrix2 <- t(Matrix1)
+
+Add <- Matrix1 + Matrix2
+Subtract <- Matrix1 - Matrix2
+Multiply <- Matrix1 * Matrix2
+Divide <- Matrix1 / Matrix2
+
+print(Matrix1)
+print(Matrix2)
+print(Add)
+print(Subtract)
+print(Multiply)
+print(Divide)
