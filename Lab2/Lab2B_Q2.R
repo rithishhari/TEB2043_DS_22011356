b2faa4355730ebf2f1cdb43d1a26baa24d09e504
Rithish Hari <hiimrithish@gmail.com>
2026-02-23 01:37:04 +0800

    Update Lab2B_Q2.R

--- Lab2/Lab2B_Q2.R
+++ Lab2/Lab2B_Q2.R
@@ -1,11 +1,6 @@
-str1 <- readline("Enter first string: ")
-str2 <- readline("Enter second string: ")
+str1 <- readline("Enter string 1: ")
+str2 <- readline("Enter string 2: ")
 
-str1_lower <- tolower(str1)
-str2_lower <- tolower(str2)
+result <- tolower(str1) == tolower(str2)
 
-if (str1_lower == str2_lower) {
-  cat("The strings are the same")
-} else {
-  cat("The strings are different")
-}
+cat("This program compare 2 strings. Both inputs are similar:", result)
